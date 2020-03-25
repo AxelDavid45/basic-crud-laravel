17e263de1bccb9411fe940d9847bac301b0b38e0
Axel Espinosa <axeldavid45@gmail.com>
2020-03-25 16:57:53 +0000

    Agregamos el formulario para que puedan agregar datos a la db

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -17,6 +17,33 @@
 <main class="container">
     <div class="row">
         <div class="col-sm-8 mx-auto">
+            <div class="card border-0 my-3 shadow">
+                <div class="card-body">
+                    <h1 class="text-center">CRUD</h1>
+                    <form class="form-row d-flex justify-content-center"
+                          action="{{ route('users.store') }}"
+                          method="POST">
+                        <div class="col-sm-3">
+                            <input type="text" class="form-control" placeholder="Name" name="name"
+                                   value="{{ old('name') }}">
+                        </div>
+                        <div class="col-sm-3">
+                            <input type="email" class="form-control" placeholder="Email"
+                                   name="email"
+                                   value="{{ old('email') }}">
+                        </div>
+                        <div class="col-sm-3">
+                            <input type="password" class="form-control" placeholder="Password"
+                                   name="password"
+                                   value="{{ old('password') }}">
+                        </div>
+                        <div class="col-auto">
+                            @csrf
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
             <table class="table">
                 <thead>
                 <tr>
